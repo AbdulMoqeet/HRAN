f0df38e89b5c6d0d599fdd4a4a235dc86ffd2837
Abdul Muqeet <7454802+AbdulMoqeet@users.noreply.github.com>
2019-10-24 12:41:01 +0900

    Test codes

--- HRAN/src/demo.sh
+++ HRAN/src/demo.sh
@@ -1,11 +1,22 @@
 # HRAN(x2) 
 #python main.py --template HRAN --model HRAN --scale 2 --patch_size 96 --save HRAN_x2 --ext sep_reset
 
-# HRAN(x3) 
-#python main.py --template HRAN --model HRAN --scale 3 --patch_size 144 --save HRAN_x3 --ext sep_reset
-
 # HRAN(x4) 
 #python main.py --template HRAN --model HRAN --scale 4 --patch_size 192 --save HRAN_x4 --ext sep_reset
+
+# HRAN(x8) 
+#python main.py --template HRAN --model HRAN --scale 4 --patch_size 384 --save HRAN_x8 --ext sep_reset
+
+## Testing
+
+# HRAN(x2)
+python3 main.py --template HRAN --model HRAN --scale 2 --save HRAN_x2 --pre_train ../trained_models/model_best_x2.pt --test_only --save_results
+
+# HRAN(x4)
+ python3 main.py --template HRAN --model HRAN --scale 4  --save HRAN_x4 --pre_train ../trained_models/model_best_x4.pt --test_only --save_results
+
+# HRAN(x8)
+ python3 main.py --template HRAN --model HRAN --scale 8  --save HRAN_x8 --pre_train ../trained_models/model_best_x8.pt --test_only --save_results
 
 
 # Test your own images
