a5b8a0ea7faa308059d7e6b4c6b5367e5cb79245
Abdul Muqeet <7454802+AbdulMoqeet@users.noreply.github.com>
2019-10-24 11:33:23 +0900

    Fix typos

--- HRAN/src/demo.sh
+++ HRAN/src/demo.sh
@@ -1,5 +1,5 @@
 # HRAN(x2) 
-#python main.py --template HRA --model HRAN --scale 2 --patch_size 96 --save HRAN_x2 --ext sep_reset
+#python main.py --template HRAN --model HRAN --scale 2 --patch_size 96 --save HRAN_x2 --ext sep_reset
 
 # HRAN(x3) 
 #python main.py --template HRAN --model HRAN --scale 3 --patch_size 144 --save HRAN_x3 --ext sep_reset
